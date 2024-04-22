67c2a86f599df13d5da50f4246e698ec917c35ea
Alexis Gonzalez <1991.alexis@gmail.com>
2024-04-21 20:12:31 -0400

    can read info from csv and crawl the main image from article page

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const fs = require('fs');
 const csv = require('csv-parser');
+const cheerio = require('cheerio');
+const axios = require('axios');
+
 const inputFilePath = './InventarioAuraMakeupCaja1.csv';
+const elemSelector =  'body > #tt-pageContent > .shopify-section > .product_page_template > link';
 let readData = [];
 
 fs.createReadStream(inputFilePath) 
@@ -12,14 +16,25 @@ fs.createReadStream(inputFilePath)
     } catch (err) {
       console.log('erronio', err);
     }
-  }).on('end', () => {
-    console.log('Terminé de leer la data', new Date());
+  }).on('end', async () => {
+    console.log('Finished reading -> Starting Crawling', new Date());
+    
+    let $;
+    readData.forEach((item, index) => {
+      setTimeout(async () => {
+        await axios(item.Link).then((response) => {
+          $ = cheerio.load(response.data); 
+          item.image = $(elemSelector)[0].attribs.content;
+          console.log(item.image);
+        });
+      }, index * 500); // 5000 milliseconds (5 seconds) delay between each iteration
+    })
   });
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.status(200).send(readData);
 });
 
