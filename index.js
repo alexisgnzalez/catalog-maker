e518af1337c1eaeffcf8457512dc15cfd9df5a82
Alexis Gonzalez <1991.alexis@gmail.com>
2024-07-30 18:47:07 -0400

    now getting info from raw jason

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const express = require('express');
-const fs = require('fs');
-const csv = require('csv-parser');
-const cheerio = require('cheerio');
-const axios = require('axios');
-
-const inputFilePath = './InventarioAuraMakeupCaja1.csv';
-const elemSelector =  'body > #tt-pageContent > .shopify-section > .product_page_template > link';
+const cors = require('cors');
 let readData = [];
 
-const data = require('./RawInfoCatalogoMakeup.json');
-console.log(data);
+readData = require('./RawInfoCatalogoMakeup.json');
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.get("/", async (req, res) => {
   res.status(200).send(readData);
