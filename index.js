c6d127321358b4ae1d5875752348fcfd6776c1fd
Alexis Gonzalez <1991.alexis@gmail.com>
2024-07-30 18:24:46 -0400

    now reading raw json data

--- index.js
+++ index.js
@@ -8,28 +8,8 @@ const inputFilePath = './InventarioAuraMakeupCaja1.csv';
 const elemSelector =  'body > #tt-pageContent > .shopify-section > .product_page_template > link';
 let readData = [];
 
-fs.createReadStream(inputFilePath) 
-  .pipe(csv())
-  .on('data', (data) => {
-    try {
-      readData.push(data);
-    } catch (err) {
-      console.log('erronio', err);
-    }
-  }).on('end', async () => {
-    console.log('Finished reading -> Starting Crawling', new Date());
-    
-    let $;
-    readData.forEach((item, index) => {
-      setTimeout(async () => {
-        await axios(item.Link).then((response) => {
-          $ = cheerio.load(response.data); 
-          item.image = $(elemSelector)[0].attribs.content;
-          console.log(item.image);
-        });
-      }, index * 500); // 5000 milliseconds (5 seconds) delay between each iteration
-    })
-  });
+const data = require('./RawInfoCatalogoMakeup.json');
+console.log(data);
 
 const app = express();
 app.use(express.json());
